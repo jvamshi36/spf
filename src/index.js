22c477a898c412e350a22eec14fb63932c2632f0
jvamshi36 <jvamshi36@gmail.com>
2025-06-04 09:19:33 +0530

    fix the error of import ../../frontend/src/App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './tailwind.output.css';
-import App from '../../frontend/src/App';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import * as Sentry from '@sentry/react';
